4d5efe002790fc697f09c8dddf1a2fb3c6300ff4
zaaraseemeen1 <zseemeen2007@gmail.com>
2026-06-09 09:08:19 -0400

    code cleanup

--- classify_emails.py
+++ classify_emails.py
@@ -2,35 +2,35 @@ import csv
 
 important = ["urgent", "sign", "contract", "review", "board meeting", "NDA", "introduction", "allocation", "deadline", "approval", "action required"]
 
-noise = ["newsletter", "webinar", "invoice", "prize", "click here", "daily summary", "morning note", "unsubscribe", "reminder", "deals", "unsubscribe", "click here", "notification", "viewed your profile", "weekly", "browse"]
+noise = ["newsletter", "webinar", "invoice", "prize", "click here", "daily summary", "morning note", "reminder", "deals", "unsubscribe", "notification", "viewed your profile", "weekly", "browse"]
 
-sender_domains = ["ir@blackrock.com", "s.mitchell@granthamlaw.com", "chen@alphaventures.vc", "boardsec@nexuscorp.com", "legal@oriongroup.com"]
+sender_domains = ["ir@blackrock.com", "s.mitchell@granthamlaw.com", "d.chen@alphaventures.vc", "boardsec@nexuscorp.com", "legal@oriongroup.com"]
 
 def classify(sender, subject, body):
     # combined subject and body
     comb = (subject + " " + body).lower()
 
-    # is the sender in the important sender's list? ASK ABOUT THIS
+    # is the sender in the important sender's list?
     for email in sender_domains:
-        if (sender == email):
+        if (sender.lower() == email.lower()):
             return "Important"
-        
-    # does contain a noise keyword?
-    for word in noise:
-        if (word in comb):
-            return "Noise"
     
     # does contain important keyword?
     for word in important:
         if (word in comb):
             return "Important"
         
+    # does contain a noise keyword?
+    for word in noise:
+        if (word in comb):
+            return "Noise"
+        
     return "Normal"
 
 def main():
-    important = 0
-    noise = 0
-    normal = 0
+    important_count = 0
+    noise_count = 0
+    normal_count = 0
 
     # with keyowrd takes care of closing file for me
     with open("inbox.csv") as f:
@@ -39,15 +39,15 @@ def main():
             result = classify(row["sender_email"],row["subject"], row["body_preview"])
             print(result + " " + row["sender_name"] + " " + row["subject"])
             if (result == "Important"):
-                important = important + 1
+                important_count = important_count + 1
             elif (result == "Noise"):
-                noise = noise +1
+                noise_count = noise_count +1
             else:
-                normal = normal +1 
+                normal_count = normal_count +1 
         
-        print(f"Number of emails classified as important: {important}")
-        print(f"Number of emails classified as normal: {normal}")
-        print(f"Number of emails classified as noise: {noise}")
+        print(f"Number of emails classified as important: {important_count}")
+        print(f"Number of emails classified as normal: {normal_count}")
+        print(f"Number of emails classified as noise: {noise_count}")
 
 
 main()
