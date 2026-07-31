7d701443b8a281eb9997e71201a57d3691335bc1
zaaraseemeen1 <zseemeen2007@gmail.com>
2026-07-30 23:41:48 -0400

    finsihed ResultDisplay.jsx

---
+++ email-classifier-ui/src/components/ResultDisplay.jsx
@@ -0,0 +1,16 @@
+const colorMapper = {
+    Important: "#1B7F37",
+    Normal: "#6B7280",
+    Noise: "#C1272D"
+}
+
+function ResultDisplay(prop) {
+
+    if (prop.result == null) {
+        return null;
+    } else {
+        return (
+            <h1 style = {{color: colorMapper[prop.result]}}>{prop.result}</h1>
+        );
+    }
+}
